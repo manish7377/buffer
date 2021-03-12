e301ba0646959a0d092b58f93125db5c166bd15e
meera <shu@ahb.com>
2021-03-12 22:29:46 +0530

    project

--- Javascript/resources.js
+++ Javascript/resources.js
@@ -1,5 +1,5 @@
 function btn(){
-    location.assign("file:///C:/Users/91690/Documents/JS/unit%202%20project%20buffer/Buffer/HTML/subscribe.html")
+    location.assign("/Users/kaustubhmendhe/Documents/unit2/project/buffer/Buffer/HTML/subscribe.html")
     console.log("hi")
 }
  
